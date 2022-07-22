9a99b2e082ea66b7daf2d34a2125812ef483bbbe
priteshpatel <pritesh.patel@appliedmedical.com>
2022-07-22 09:16:49 -0700

    Fixing Auto Update V2

--- webapp/service-worker.js
+++ webapp/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pwa-ui5-priteshprofile-v1.0.4';
+const CACHE_NAME = 'pwa-ui5-priteshprofile-v1.0.5';
 const RESOURCES_TO_PRELOAD = [
     'index.html',
     'logo.svg',
@@ -28,30 +28,93 @@ const resourcesToCache = RESOURCES_TO_PRELOAD.concat([
 ]);
 
 // Preload some resources during install
-self.addEventListener('install', function (event) {
+// self.addEventListener('install', function (event) {
+//     self.skipWaiting();
+//     event.waitUntil(
+//         caches.open(CACHE_NAME).then(function (cache) {
+//             return cache.addAll(RESOURCES_TO_PRELOAD);
+//             // if any item isn't successfully added to
+//             // cache, the whole operation fails.
+//         }).catch(function (error) {
+//             console.error(error);
+//         })
+//     );
+// });
+
+self.addEventListener('install', (event) => {
+    // prevents the waiting, meaning the service worker activates
+    // as soon as it's finished installing
+    // NOTE: don't use this if you don't want your sw to control pages
+    // that were loaded with an older version
     self.skipWaiting();
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(function (cache) {
-            return cache.addAll(RESOURCES_TO_PRELOAD);
-            // if any item isn't successfully added to
-            // cache, the whole operation fails.
-        }).catch(function (error) {
-            console.error(error);
-        })
-    );
-});
+  
+    event.waitUntil((async () => {
+      try {
+        // self.cacheName and self.contentToCache are imported via a script
+        const cache = await caches.open(self.cacheName);
+        const total = self.contentToCache.length;
+        let installed = 0;
+  
+        await Promise.all(self.contentToCache.map(async (url) => {
+          let controller;
+  
+          try {
+            controller = new AbortController();
+            const { signal } = controller;
+            // the cache option set to reload will force the browser to
+            // request any of these resources via the network,
+            // which avoids caching older files again
+            const req = new Request(url, { cache: 'reload' });
+            const res = await fetch(req, { signal });
+  
+            if (res && res.status === 200) {
+              await cache.put(req, res.clone());
+              installed += 1;
+            } else {
+              console.info(`unable to fetch ${url} (${res.status})`);
+            }
+          } catch (e) {
+            console.info(`unable to fetch ${url}, ${e.message}`);
+            // abort request in any case
+            controller.abort();
+          }
+        }));
+  
+        if (installed === total) {
+          console.info(`application successfully installed (${installed}/${total} files added in cache)`);
+        } else {
+          console.info(`application partially installed (${installed}/${total} files added in cache)`);
+        }
+      } catch (e) {
+        console.error(`unable to install application, ${e.message}`);
+      }
+    })());
+  });
 
 // Delete obsolete caches during activate
-self.addEventListener('activate', function (event) {
-    event.waitUntil(
-        caches.keys().then(function (keyList) {
-            return Promise.all(keyList.map(function (key) {
-                if (key !== CACHE_NAME) {
-                    return caches.delete(key);
-                }
-            }));
-        })
-    );
+// self.addEventListener('activate', function (event) {
+//     event.waitUntil(
+//         caches.keys().then(function (keyList) {
+//             return Promise.all(keyList.map(function (key) {
+//                 if (key !== CACHE_NAME) {
+//                     return caches.delete(key);
+//                 }
+//             }));
+//         })
+//     );
+// });
+
+// remove old cache if any
+self.addEventListener('activate', (event) => {
+    event.waitUntil((async () => {
+      const cacheNames = await caches.keys();
+  
+      await Promise.all(cacheNames.map(async (cacheName) => {
+        if (self.cacheName !== cacheName) {
+          await caches.delete(cacheName);
+        }
+      }));
+    })());
 });
 
 // During runtime, get files from cache or -> fetch, then save to cache
